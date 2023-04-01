75108958222054ff78ca42e262d54c1f1a465c26
anajullia <anajullia@anajullia.com>
2023-04-01 15:37:55 -0300

    versão inicial

---
+++ script.js
@@ -0,0 +1,23 @@
+var Mapas = ["Lotus", "Pearl", "Fracture", "Breeze", "Icebox", "Bind", "Haven", "Split", "Ascent"];
+
+var Agentes = ["Gekko", "Harbor", "Fade", "Neon", "Chamber", "KAY/O", "Astra", "Yoru", "Skye", "Raze", "Jett", "Omen", "Breach", "Killjoy", "Reyna", "Cypher", "Viper", "Sova", "Sage", "Phoenix", "Brimstone"];
+
+
+
+// lado esquerdo
+
+function corumsecum(){
+    document.getElementById("sectionagentes").style.backgroundColor='#1B2D40';;
+}
+
+function cordoissecum(){
+    document.getElementById("sectionagentes").style.backgroundColor='#0F1923';;
+    document.getElementById("sectionmapas").style.backgroundColor='#0F1923';;
+}
+
+function btnesquerda(){
+    var loopagentes = document.getElementById("loopagentes");
+    loopagentes.innerHTML = Mapas;
+
+}
+// fim do lado esquerdo
